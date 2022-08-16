667056fb416a48aa43ad3dfd2fc12d3214893d8e
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-08-16 12:28:30 +0100

    .gitignore fix

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+// const pictureContainer = document.querySelector(".picture-container");
+
+// // get current width and height based on window size;
+// var ro = new ResizeObserver(entries => {
+//   for (let entry of entries) {
+//     const cr = entry.contentRect;
+//     // set css variables accordingly
+//     pictureContainer.style.setProperty("--width", cr.width);
+//     pictureContainer.style.setProperty("--height", cr.height);
+//   }
+// });
+
+// // Observe element for changes
+// ro.observe(pictureContainer);
